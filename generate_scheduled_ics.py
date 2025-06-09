e09e8043c718ff6c51f24fffb7842ce064d868b3
Edith Pigon <epigon@ucsd.edu>
2025-06-09 09:23:09 -0700

    Create batch file that triggers generate_scheduled_ics()

---
+++ generate_scheduled_ics.py
@@ -0,0 +1,11 @@
+from app import create_app
+from app.scheduler.routes import generate_scheduled_ics
+
+app = create_app()
+
+def generate_ics_with_app_context():
+    with app.app_context():
+       generate_scheduled_ics()
+
+if __name__ == '__main__':
+    generate_ics_with_app_context()  
--- run.py
+++ run.py
@@ -1,30 +1,8 @@
 from app import create_app
-from app.scheduler.routes import generate_scheduled_ics
 from flask_bootstrap import Bootstrap5
-from flask_apscheduler import APScheduler 
 
 app = create_app()
 bootstrap = Bootstrap5(app)
 
-class Config:
-    JOBS = [
-        {
-            "id": "job1",
-            "func": "run:run_generate_scheduled_ics",          
-            "trigger": "interval",
-            "minutes": 2,
-        }
-    ]
-
-    SCHEDULER_API_ENABLED = True
-
-def run_generate_scheduled_ics():
-    with app.app_context():
-        generate_scheduled_ics()
-
 if __name__ == '__main__':
-    app.config.from_object(Config())    
-    scheduler = APScheduler()   
-    scheduler.init_app(app)
-    scheduler.start()
     app.run(host="0.0.0.0",debug=False,threaded=True) 
