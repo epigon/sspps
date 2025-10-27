388f58b9188e1eb01f58fc8e206723c9cf5183a2
epigon <epigon@ucsd.edu>
2025-10-27 14:20:18 -0700

    Add login_required to routes

--- app/routes/emma_service.py
+++ app/routes/emma_service.py
@@ -2,21 +2,13 @@ from app.emmaAPIAdapter import EmmaAPIAdapter
 from app.utils import permission_required
 from flask import render_template, request, Blueprint, jsonify
 from flask_login import login_required, current_user
-import base64
-import os
-import requests
 
 bp = Blueprint('emma', __name__, url_prefix='/emma')
 
 adapter = EmmaAPIAdapter() 
 
-# Routes to Webpages
-@bp.before_request
+@bp.route('/groups', methods=['GET', 'POST'])
 @login_required
-def before_request():
-    pass
-
-@bp.route("/groups")
 @permission_required('listserv+view')
 def groups_page():
     groups = adapter.get_groups() or []
@@ -25,12 +17,14 @@ def groups_page():
     return render_template('emma/list_groups.html', groups=groups)
 
 @bp.route("/groups/<int:group_id>/members")
+@login_required
 @permission_required('listserv+view')
 def group_members_page(group_id):
     group = next((g for g in adapter.get_groups() if g["member_group_id"] == group_id), None)
     return render_template("emma/list_members.html", group=group)
 
 @bp.route("/api/groups/<int:group_id>/members")
+@login_required
 def group_members_api(group_id):
     members = adapter.get_group_members(group_id) or []
     members = sorted(members, key=lambda m: (m.get("email") or "").lower())
