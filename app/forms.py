34339ab930d7febb5a7f5d2cbb02575968f1215e
epigon <epigon@ucsd.edu>
2026-03-28 20:09:46 -0700

    Change submit_btn in form

--- app/forms.py
+++ app/forms.py
@@ -308,7 +308,7 @@ class InstrumentRequestForm(FlaskForm):
     requestor_name = StringField("Requestor Name", validators=[DataRequired()])
     requestor_email = StringField("Requestor Email", validators=[DataRequired(), Email()])
 
-    # submit = SubmitField("Submit Request")
+    submit_btn = SubmitField("Submit", render_kw={"class": "btn btn-primary"})
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
